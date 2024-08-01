9b97ed3699070fc27fae96a12c9a7420eda9c278
Niloy74779 <mdniloy74779@gmail.com>
2024-08-01 11:01:18 +0600

    Notification

--- notification/models.py
+++ notification/models.py
@@ -7,9 +7,6 @@ from interview.models import InterviewBooking
 
 class notification(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    visa_application = models.ForeignKey(VisaApplication, on_delete=models.CASCADE)
-    visa_status = models.ForeignKey(VisaStatus, on_delete=models.CASCADE)
-    interview_booking = models.ForeignKey(InterviewBooking, on_delete=models.CASCADE)
     massage = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     
