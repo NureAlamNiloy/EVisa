de741c0d95f17c51b3b15a12a878bd81d7419a07
Niloy74779 <mdniloy74779@gmail.com>
2024-08-20 16:40:44 +0600

    File name change

--- core/settings.py
+++ core/settings.py
@@ -137,9 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
