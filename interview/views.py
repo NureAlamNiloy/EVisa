49dea8b79f0cb8ea3e70a6c694ed21f23959eb0a
Niloy74779 <mdniloy74779@gmail.com>
2024-09-05 12:15:10 +0600

    No interview API Success

--- interview/views.py
+++ interview/views.py
@@ -1,17 +1,34 @@
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import  status
 from rest_framework.views import APIView
-from django.db.models import Count
 from .models import Appointment, NoInterview, ScheduleSlot
 from .serializer import AppointmentSerializer, NoInterviewSerializer, ScheduleSlotSerializer
-from rest_framework.permissions import IsAuthenticated 
-
+from datetime import datetime, timedelta, time
 
 
 # Create your views here.
 
+
+
 class NoInterviewViewset(APIView):
-    pass
+    def post(self, request):
+        serializer = NoInterviewSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            no_interview_dates = [item['no_interview_date'] for item in serializer.validated_data]
+        
+            for date in no_interview_dates:
+                NoInterview.objects.get_or_create(no_interview_date=date)
+            ScheduleSlot.objects.filter(interview_date__in=no_interview_dates).delete()
+
+            return Response({'message': 'No-interview dates updated successfully!'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def get(self, request):
+        no_interview_dates = NoInterview.objects.all()
+        serializer = NoInterviewSerializer(no_interview_dates, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 
 class ScheduleSlotViewset(APIView):
