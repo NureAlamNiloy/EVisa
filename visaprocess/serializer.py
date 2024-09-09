c9b15a0d9728a5177f2db15b4f06ea73edefdcd8
Niloy74779 <mdniloy74779@gmail.com>
2024-09-09 12:33:00 +0600

    specific application date and time

--- visaprocess/serializer.py
+++ visaprocess/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import VisaApplication, VisaStatus
-from interview.serializer import AppointmentSerializer
-from interview.models import Appointment
+from interview.serializer import AppointmentSerializer, ScheduleSlotSerializer
+from interview.models import Appointment, ScheduleSlot
 import base64
 
 class VisaStatusSerializer(serializers.ModelSerializer):
@@ -12,9 +12,10 @@ class VisaStatusSerializer(serializers.ModelSerializer):
 class VisaApplicationSerializer(serializers.ModelSerializer):
     
     visa_statuses = VisaStatusSerializer(many=True, read_only=True)
-    # Appointment module ee visaApplication er relation ee related name appointment set kora ase tai ei filed diye serializer diye application er moddhe specific appointment dekhano jaitasee
-    appointment = AppointmentSerializer(many=True, read_only=True) 
     encoded_id = serializers.SerializerMethodField()
+    # Appointment module ee visaApplication er relation ee related name appointment set kora ase tai ei filed diye serializer diye application er moddhe specific appointment dekhano jaitasee
+    interview_date = serializers.SerializerMethodField()
+    interview_start_time = serializers.SerializerMethodField()
     
     class Meta:
         model = VisaApplication
@@ -25,7 +26,17 @@ class VisaApplicationSerializer(serializers.ModelSerializer):
         encoded_id = base64.urlsafe_b64encode(str(obj.id).encode())
         return encoded_id
    
+    def get_interview_date(self, obj):
+        appointment = Appointment.objects.filter(visa_application=obj).first()
+        if appointment and appointment.schedule_slot:
+            return appointment.schedule_slot.interview_date
+        return None
 
+    def get_interview_start_time(self, obj):
+        appointment = Appointment.objects.filter(visa_application=obj).first()
+        if appointment and appointment.schedule_slot:
+            return appointment.schedule_slot.start_time
+        return None
 
 
 
