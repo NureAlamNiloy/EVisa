2f0b18c255b857ba21d68eed9e812a40cfed22e9
Niloy74779 <mdniloy74779@gmail.com>
2024-09-18 14:38:05 +0600

    Report error solved

--- report/views.py
+++ report/views.py
@@ -1,29 +1,10 @@
-from django.db.models.functions import TruncYear, TruncMonth, TruncWeek
+from collections import defaultdict
 from django.utils import timezone
 from rest_framework.views import APIView
 from visaprocess.models import VisaApplication, VisaStatus
 from rest_framework.response import Response
 from django.db.models import Count
-
-
-# Specific date filtering report.
-# class VisaTypeReportView(APIView):
-#     def post(self, request):
-#         start_date = request.data.get('start_date')
-#         end_date = request.data.get('end_date')
-
-#         # Filter VisaApplication by the date range if provided
-#         if start_date and end_date:
-#             visa_applications = VisaApplication.objects.filter(submission_date__range=[start_date, end_date])
-#         else:
-#             visa_applications = VisaApplication.objects.all()
-
-#         visatype_weekly = visa_applications.annotate(week=TruncWeek('submission_date')).values('week', 'visa_type').annotate(count=Count('id'))
-#         data = {
-#             "total" : list(visatype_weekly),
-#         }
-#         return Response(data)
-
+from datetime import datetime
 
 class VisaTypeReportView(APIView):
     def get(self, request):
@@ -33,28 +14,42 @@ class VisaTypeReportView(APIView):
         start_of_last_year = now.replace(month=1, day=1) - timezone.timedelta(days=1)
         start_of_last_year = start_of_last_year.replace(month=1, day=1)
 
-        visatype_weekly = VisaApplication.objects.filter(submission_date__gte=start_of_last_week).annotate(week=TruncWeek('submission_date')).values('week', 'visa_type').annotate(count=Count('id'))
-        visatype_monthly = VisaApplication.objects.filter(submission_date__gte=start_of_last_month).annotate(month=TruncMonth('submission_date')).values('month', 'visa_type').annotate(count=Count('id'))
-        visatype_yearly = VisaApplication.objects.filter(submission_date__gte=start_of_last_year).annotate(year=TruncYear('submission_date')).values('year', 'visa_type').annotate(count=Count('id'))
+        visa_apps = VisaApplication.objects.filter(submission_date__gte=start_of_last_year)
+        weekly_grouping = defaultdict(lambda: defaultdict(int))
+        monthly_grouping = defaultdict(lambda: defaultdict(int))
+        yearly_grouping = defaultdict(lambda: defaultdict(int))
+        
+        for app in visa_apps:
+            if app.submission_date >= start_of_last_week:
+                week = app.submission_date.strftime('%Y-%W')
+                weekly_grouping[app.visa_type][week] += 1
+
+
+            if app.submission_date >= start_of_last_month:
+                month = app.submission_date.strftime('%Y-%m')
+                monthly_grouping[app.visa_type][month] += 1
+
+            year = app.submission_date.strftime('%Y')
+            yearly_grouping[app.visa_type][year] += 1
+            
         visatype_total = VisaApplication.objects.values('visa_type').annotate(count=Count('id'))
 
         data = {
-            "total" : list(visatype_total),
-            "last_week": list(visatype_weekly),
-            "last_month": list(visatype_monthly),
-            "last_year": list(visatype_yearly),
+            "total": list(visatype_total),
+            "last_week": weekly_grouping,
+            "last_month": monthly_grouping,
+            "last_year": yearly_grouping,
         }
         return Response(data)
-
 
 class VisaStatusReportView(APIView):
     def get(self, request):
         visaStatus_count = VisaStatus.objects.values('visa_status').annotate(count=Count('id'))
-        all_statuses = ['AdminApprove', 'Pending','PoliceVerification','Approved']
+        all_statuses = ['AdminApprove', 'Pending', 'PoliceVerification', 'Approved']
         status_count_dict = {item['visa_status']: item['count'] for item in visaStatus_count}
         data = {status: status_count_dict.get(status, 0) for status in all_statuses}
-        return Response(data )
- 
+        return Response(data)
+
 class ApproveRejectReportView(APIView):
     def get(self, request):
         approve_count = VisaApplication.objects.filter(is_approved=True).count()
@@ -63,12 +58,10 @@ class ApproveRejectReportView(APIView):
         under_modified = VisaApplication.objects.filter(is_modified=True).count()
 
         data = {
-            'total_approve' : approve_count,
-            'total_reject' : reject_count,
-            'total_application' : total_application,
-            'under_modified' : under_modified
+            'total_approve': approve_count,
+            'total_reject': reject_count,
+            'total_application': total_application,
+            'under_modified': under_modified
         }
 
         return Response(data)
-
-
